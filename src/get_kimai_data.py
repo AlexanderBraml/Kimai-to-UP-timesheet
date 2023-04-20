1d8c03a9863c049c23c11ed61681758bfd28b617
Alexander Braml <braml@fim.uni-passau.de>
2023-04-20 17:27:27 +0200

    Implement api as data source

--- src/get_kimai_data.py
+++ src/get_kimai_data.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 from typing import List, Tuple
 
+import requests
 from dateutil.parser import parse
 
 DATE_INDEX = 0
@@ -19,5 +20,30 @@ def read_csv(path: str) -> List[Tuple[datetime.datetime, datetime.datetime]]:
             start = datetime.datetime.combine(date, parse(row[START_TIME_INDEX]).time())
             end = datetime.datetime.combine(date, parse(row[END_TIME_INDEX]).time())
             times.append((start, end))
+
+    return times
+
+
+def read_api(url: str, user: str, token: str, activity: int, month: int) \
+        -> List[Tuple[datetime.datetime, datetime.datetime]]:
+    headers = {
+        'X-AUTH-USER': user,
+        'X-AUTH-TOKEN': token,
+    }
+    params = {
+        'size': 999999,
+        'activities': [activity],
+    }
+
+    response = requests.get(url + '/api/timesheets', headers=headers, params=params)
+    result = response.json()
+
+    times = []
+    for entry in result:
+        start_date = parse(entry['begin']).replace(tzinfo=None)
+        end_date = parse(entry['end']).replace(tzinfo=None)
+        if start_date.year == datetime.datetime.now().year and int(start_date.month) == month:
+            times.append((start_date, end_date))
+    times.sort(key=lambda x: x[0])
 
     return times
