0e5eadd8de9e269472d5969c51cdf7158a2b1b48
Alexander Braml <braml@fim.uni-passau.de>
2023-04-20 10:07:38 +0200

    Implement function to retrieve kimai data

---
+++ src/get_kimai_data.py
@@ -0,0 +1,28 @@
+import csv
+import datetime
+from typing import List, Tuple
+
+from dateutil.parser import parse
+
+DATE_INDEX = 0
+START_TIME_INDEX = 1
+END_TIME_INDEX = 2
+
+
+def read_csv(path: str) -> List[Tuple[datetime.datetime, datetime.datetime]]:
+    times = []
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)  # Skip header
+        for row in reader:
+            date = parse(row[DATE_INDEX]).date()
+            start = datetime.datetime.combine(date, parse(row[START_TIME_INDEX]).time())
+            end = datetime.datetime.combine(date, parse(row[END_TIME_INDEX]).time())
+            times.append((start, end))
+
+    return times
+
+
+if __name__ == '__main__':
+    r = read_csv('../templates/source.csv')
+    pass
