137dd491de50689c10e366d7563eb17718d0b669
Alexander Braml <braml@fim.uni-passau.de>
2023-04-20 10:58:11 +0200

    Refactor write function

--- src/write_target.py
+++ src/write_target.py
@@ -5,36 +5,37 @@ from openpyxl import load_workbook
 
 from src.get_kimai_data import read_csv
 
-date_index = (5, 12)
+DATE_INDEX = (5, 12)
 
-time_row = 12
+TIME_ROW = 12
 
-date_col_1 = 1
-start_hour_col_1 = 2
-start_min_col_1 = 4
-end_hour_col_1 = 7
-end_min_col_1 = 9
+DATE_COL = 1
+START_HOUR_COL = 2
+START_MIN_COL = 4
+END_HOUR_COL = 7
+END_MIN_COL = 9
 
-max_rows = 31
+MAX_ROWS = 31
 
 
 def write_to_file(target_template: str, target_file: str,
                   times: List[Tuple[datetime.datetime, datetime.datetime]]) -> None:
-    assert len(times) > 0
+    assert 0 < len(times) <= 2 * MAX_ROWS
 
     workbook = load_workbook(filename=target_template)
     sheet = workbook.active
 
-    sheet.cell(*date_index).value = times[0][0].replace(day=1)
+    sheet.cell(*DATE_INDEX).value = times[0][0].replace(day=1)
 
     for idx, time in enumerate(times):
-        row = time_row + idx
-        sheet.cell(row, date_col_1).value = time[0].strftime("%d.%m.%Y")
+        row = TIME_ROW + (idx % MAX_ROWS)
+        col_offset = 12 if idx >= MAX_ROWS else 0
 
-        sheet.cell(row, start_hour_col_1).value = time[0].hour
-        sheet.cell(row, start_min_col_1).value = time[0].minute
-        sheet.cell(row, end_hour_col_1).value = time[1].hour
-        sheet.cell(row, end_min_col_1).value = time[1].minute
+        sheet.cell(row, DATE_COL + col_offset).value = time[0].strftime("%d.%m.%Y")
+        sheet.cell(row, START_HOUR_COL + col_offset).value = time[0].hour
+        sheet.cell(row, START_MIN_COL + col_offset).value = time[0].minute
+        sheet.cell(row, END_HOUR_COL + col_offset).value = time[1].hour
+        sheet.cell(row, END_MIN_COL + col_offset).value = time[1].minute
 
     workbook.save(target_file)
     workbook.close()
