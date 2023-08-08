90157da6a1c0bcf6662c94b1ca6d77950d3490d1
Alexander Braml <braml@fim.uni-passau.de>
2023-08-08 16:22:10 +0200

    Fix bug when there are no times

--- src/write_target.py
+++ src/write_target.py
@@ -23,17 +23,18 @@ def write_to_file(target_template: str, target_file: str,
     workbook = load_workbook(filename=target_template)
     sheet = workbook.active
 
-    sheet.cell(*DATE_INDEX).value = times[0][0].replace(day=1)
+    if len(times) > 0:
+        sheet.cell(*DATE_INDEX).value = times[0][0].replace(day=1)
 
-    for idx, time in enumerate(times):
-        row = TIME_ROW + (idx % MAX_ROWS)
-        col_offset = 12 if idx >= MAX_ROWS else 0
-
-        sheet.cell(row, DATE_COL + col_offset).value = time[0].strftime("%d.%m.%Y")
-        sheet.cell(row, START_HOUR_COL + col_offset).value = time[0].hour
-        sheet.cell(row, START_MIN_COL + col_offset).value = time[0].minute
-        sheet.cell(row, END_HOUR_COL + col_offset).value = time[1].hour
-        sheet.cell(row, END_MIN_COL + col_offset).value = time[1].minute
+        for idx, time in enumerate(times):
+            row = TIME_ROW + (idx % MAX_ROWS)
+            col_offset = 12 if idx >= MAX_ROWS else 0
+    
+            sheet.cell(row, DATE_COL + col_offset).value = time[0].strftime("%d.%m.%Y")
+            sheet.cell(row, START_HOUR_COL + col_offset).value = time[0].hour
+            sheet.cell(row, START_MIN_COL + col_offset).value = time[0].minute
+            sheet.cell(row, END_HOUR_COL + col_offset).value = time[1].hour
+            sheet.cell(row, END_MIN_COL + col_offset).value = time[1].minute
 
     workbook.save(target_file)
     workbook.close()
